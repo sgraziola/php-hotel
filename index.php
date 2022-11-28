685e8cba7dff6863a7043b0ea07f5c33b2c5d79a
sgraziola <simo.fdl@gmail.com>
2022-11-28 14:40:08 +0100

    PHP Hotel: Add hotels cards with infos

--- index.php
+++ index.php
@@ -43,13 +43,13 @@ $hotels = [
 ];
 
 
-foreach ($hotels as $hotel) {
+/* foreach ($hotels as $hotel) {
     echo $hotel['name'] . '<br>';
     echo $hotel['description'] . '<br>';
     echo $hotel['parking'] . '<br>';
     echo $hotel['vote'] . '<br>';
     echo $hotel['distance_to_center'] . '<br>';
-}
+} */
 
 ?>
 
@@ -60,11 +60,43 @@ foreach ($hotels as $hotel) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BOOLEAN HOTELS</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: cadetblue;
+        }
+    </style>
 </head>
 
 <body>
+    <div class="container">
+        <h1 class="my-5 pb-3 text-center">I MIGLIORI BOOLEAN HOTELS</h1>
+        <div class="row row-cols-3 gy-3">
+            <?php foreach ($hotels as $hotel) : ?>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Nome: <?= $hotel['name'] ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Chi siamo: <?= $hotel['description'] ?></h6>
+                            <p class="card-text">Parcheggio: <?php if ($hotel['parking']) {
+                                                                    echo 'Disponibile';
+                                                                } else {
+                                                                    echo 'Non disponibile';
+                                                                } ?>
+                            </p>
+                            <p class="card-text">Punteggio: <?= $hotel['vote'] ?>/ 5 </p>
+                            <p class="card-text">Distanza dal centro: <?= $hotel['distance_to_center'] ?> km </p>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
 
+    <!-- Boostrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
