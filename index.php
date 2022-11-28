7dfb79b2ea3126654a3ce86e07764c984b246244
sgraziola <simo.fdl@gmail.com>
2022-11-28 14:55:07 +0100

    PHP Hotel: Add form

--- index.php
+++ index.php
@@ -73,6 +73,21 @@ $hotels = [
 <body>
     <div class="container">
         <h1 class="my-5 pb-3 text-center">I MIGLIORI BOOLEAN HOTELS</h1>
+        <form action="index.php" method="get">
+            <div class="mb-3">
+                <label for="hotel_name" class="form-label">Cerca per nome:</label>
+                <input type="text" class="form-control" id="hotel_name" name="hotel_name">
+            </div>
+            <div class="mb-3">
+                <label for="rating" class="form-label">Cerca per punteggio:</label>
+                <input type="number" class="form-control" id="rating" name="rating">
+            </div>
+            <div class="mb-3 form-check">
+                <label class="form-check-label" for="parking">Vuoi il parcheggio?</label>
+                <input type="checkbox" class="form-check-input" id="parking" name="parking">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
         <div class="row row-cols-3 gy-3">
             <?php foreach ($hotels as $hotel) : ?>
                 <div class="col">
@@ -86,7 +101,7 @@ $hotels = [
                                                                     echo 'Non disponibile';
                                                                 } ?>
                             </p>
-                            <p class="card-text">Punteggio: <?= $hotel['vote'] ?>/ 5 </p>
+                            <p class="card-text">Punteggio: <?= $hotel['vote'] ?> / 5 </p>
                             <p class="card-text">Distanza dal centro: <?= $hotel['distance_to_center'] ?> km </p>
                         </div>
                     </div>
